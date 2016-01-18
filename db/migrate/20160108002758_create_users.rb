a8874912d44920c6aa4c99ca9cea37b4ddf5f004
Barnabas Bulpett <bbulpett@charter.net>
2016-01-17 22:01:48 -0500

    some schema mods to fix heroku pg migration issues

--- db/migrate/20160108002758_create_users.rb
+++ db/migrate/20160108002758_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :email
       t.string :username, null:false, index:true, required:true, limit:20
-      t.boolean :admin, :boolean, default: false, null: false
+      t.boolean :admin, default: false, null: false
       t.string :password_digest
 
       t.timestamps
--- db/migrate/20160118023506_remove_boolean_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBooleanFromUsers < ActiveRecord::Migration[5.0]
-  def change
-  	remove_column :users, :boolean
-  end
-end
