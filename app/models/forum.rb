351418f9de2f0c782753b1f331320d3d1ce05a1c
Barnabas Bulpett <bbulpett@charter.net>
2016-01-20 23:08:32 -0500

    Add user relationship to topics and posts

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,3 +1,8 @@
 class Forum < ApplicationRecord
 	has_many :topics, dependent: :destroy
+
+	def most_recent_post
+	  topic = Topic.last
+	  return topic
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116172125) do
+ActiveRecord::Schema.define(version: 20160121035213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160116172125) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   create_table "topics", force: :cascade do |t|
@@ -37,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160116172125) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "forum_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
