612d81fb5a39245184c06167e303ac72f3845368
Barnabas Bulpett <bbulpett@charter.net>
2016-03-11 18:38:00 -0500

    Configure local environment variables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save && verify_recaptcha(model: @user)
+    if verify_recaptcha(model: @user, timeout: 60)
+      @user.save!
       session[:user_id] = @user.id
       redirect_to root_url, notice: "Thank you for signing up!"
     else
-      render "new"
+      puts "ReCAPTCHA is FALSE!!"
+      render "new", alert: "ReCAPTCHA failed, please try again."
     end
   end
 
