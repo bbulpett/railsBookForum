c9be89ca29a2f1e02205297eba4b28cd20601a71
Barnabas Bulpett <bbulpett@charter.net>
2016-03-14 13:16:33 -0400

    Revert log in button to the old kind (lkw)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by("username = ? OR email = ?", params[:login], params[:login])
-
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
