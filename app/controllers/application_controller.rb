e58e4a9866d78cbce19d2e9ad269d26d9a9cfbc2
Barnabas Bulpett <bbulpett@charter.net>
2016-04-28 21:15:43 -0400

    Pentest 1.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  before_filter :block_ip_addresses
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -16,5 +17,13 @@ class ApplicationController < ActionController::Base
 
   def is_admin
     redirect_to root_path, alert: "Not authorized" if current_user.nil? or !current_user.admin?
+  end
+
+  def block_ip_addresses
+    head :unauthorized if current_ip_address == "24.196.174.32"
+  end
+
+  def current_ip_address
+    request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
   end
 end
